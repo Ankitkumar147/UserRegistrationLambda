5311e0302050ae5873859326008242de2f850e5d
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-08-05 09:13:32 +0530

    Added custom Exception for valid user input

--- src/com/Bridgelabz/Regex/UserRegistration.java
+++ src/com/Bridgelabz/Regex/UserRegistration.java
@@ -1,5 +1,6 @@
 package com.Bridgelabz.Regex;
 
+import javax.xml.bind.ValidationException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -36,31 +37,86 @@ public class UserRegistration {
 
     private static boolean validateFirstName(String firstName) {
         Pattern pattern = Pattern.compile("[?A-Z]+[a-z]{2,}");
+        if ( firstName == null){
+            return false;
+        }
         Matcher matcher = pattern.matcher(firstName);
+        if (matcher.matches())
+            return true;
+        else
+            try{
+                throw new ValidationException("Invalid Name");
+            } catch (ValidationException e){
+                e.printStackTrace();
+            }
         return matcher.matches();
     }
 
    private static boolean validateLastName(String lastName) {
         Pattern pattern = Pattern.compile("[?A-Z]+[a-z]{2,}");
-        Matcher matcher = pattern.matcher(lastName);
+       if ( lastName == null){
+           return false;
+       }
+       Matcher matcher = pattern.matcher(lastName);
+       if (matcher.matches())
+           return true;
+       else
+           try{
+               throw new ValidationException("Invalid Name");
+           } catch (ValidationException e){
+               e.printStackTrace();
+           }
         return matcher.matches();
     }
 
     private static boolean validateEmail(String email) {
         Pattern pattern = Pattern.compile("^[a-zA-Z]+[.][a-zA-Z]+@+[a-zA-Z]+[.][a-zA-Z][*.][*a-zA-Z]$");
+        if ( email == null){
+            return false;
+        }
         Matcher matcher = pattern.matcher(email);
+        if (matcher.matches())
+            return true;
+        else
+            try{
+                throw new ValidationException("Invalid Name");
+            } catch (ValidationException e){
+                e.printStackTrace();
+            }
         return matcher.matches();
     }
 
     private static boolean validatePhoneNumber(String phoneNumber) {
         Pattern pattern = Pattern.compile("[0-9]{2}+[-]+[0-9]{10}");
+        if ( phoneNumber == null){
+            return false;
+        }
         Matcher matcher = pattern.matcher(phoneNumber);
+        if (matcher.matches())
+            return true;
+        else
+            try{
+                throw new ValidationException("Invalid Name");
+            } catch (ValidationException e){
+                e.printStackTrace();
+            }
         return matcher.matches();
     }
 
     private static boolean validatePassword(String password) {
         Pattern pattern = Pattern.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$");
+        if ( password == null){
+            return false;
+        }
         Matcher matcher = pattern.matcher(password);
+        if (matcher.matches())
+            return true;
+        else
+            try{
+                throw new ValidationException("Invalid Name");
+            } catch (ValidationException e){
+                e.printStackTrace();
+            }
         return matcher.matches();
     }
 }
